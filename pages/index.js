46afbdc9be4420653d1371ecb4b286f1182b2032
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-11-23 13:40:11 +0530

    added server side rendering

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Header from '../components/Header'
 import Nav from '../components/Nav'
 import Results from '../components/Results'
+import requests from "../utils/requests";
 
 export default function Home() {
   return (
@@ -23,4 +24,20 @@ export default function Home() {
         
     </div>
   )
+}
+
+export async function getServerSideProps(context){
+  const genre = context.query.genre;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+    requests[genre]?.url || requests.fetchTrending.url
+    }`
+    ).then((res) => res.json());
+
+  return {
+    props: {
+      results: request.results,
+    }
+  };
 }
