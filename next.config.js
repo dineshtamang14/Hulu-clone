652eb4eb3b7ecadedda76533dd9adb05beb659bc
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-11-23 19:41:35 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   images: {
     domains: ["links.papareact.com", "image.tmdb.org"],
-  },
-  {
-  swcMinify: false // it should be false by default 
   }
+  swcMinify: false // it should be false by default 
 }
